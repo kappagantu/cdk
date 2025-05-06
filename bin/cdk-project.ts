7d3771e7151cc4ddb0d680dc0be7d507121db7b0
kappagantu <vkappagantu@yahoo.com>
2025-05-06 06:59:45 -0700

    modified to add eks cluster creation

--- bin/cdk-project.ts
+++ bin/cdk-project.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkProjectStack } from '../lib/cdk-project-stack';
+import { MasterProjectStack } from '../lib/master-project-stack';
 
 const app = new cdk.App();
-new CdkProjectStack(app, 'CdkProjectStack', {
+/*new CdkProjectStack(app, 'CdkProjectStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -17,4 +18,6 @@ new CdkProjectStack(app, 'CdkProjectStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//});
+
+new MasterProjectStack(app, 'MasterProjectStack', {});
